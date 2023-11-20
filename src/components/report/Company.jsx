0e453d664fabdacc6c53c8031e8cb02a9429071a
Venkatesh Mishra <venmishr@publicisgroupe.net>
2023-11-20 18:41:12 +0000

    added accordion for company summary
    added accordion for company summary

--- src/components/report/Company.jsx
+++ src/components/report/Company.jsx
@@ -23,33 +23,38 @@ const Company = ({ company }) => {
             <Table showTitle={false} columns={company.cash_flow.data.columns} data={company.cash_flow.data.results} />
           </div>
         </div>
-        <h3 className="fullWidth">Summary</h3>
-        <div className="row company-wrapper">
-          <div className="column">
-            <h4>Positive points</h4>
-            <ul className="actions-list">
-              {company.summary &&
-                company.summary.positive_points.map((positiveItem, index) => (
-                  <li key={index}>
-                    {positiveItem}
-                  </li>
-                ))}
-            </ul>
-          </div>
-          <div className="column">
-            <h4>Negative points</h4>
-            <ul className="actions-list">
-              {company.summary &&
-                company.summary.negative_points.map((negativeItem, index) => (
-                  <li key={index}>
-                    {negativeItem}
-                  </li>
-                ))}
-            </ul>
-          </div>
-          <div className="column">
-            <h4>Overall outlook</h4>
-            <p>{company.summary.overall_outlook}</p>
+        <div className="accordion">
+          <div className="tab company-wrapper">
+            <input type="checkbox" name="accordion-1" id="cb1" />
+            <label htmlFor="cb1" className="tab__label">Summary</label>
+            <div className="tab__content row">
+              <div className="column">
+                <h4>Positive points</h4>
+                <ul className="actions-list">
+                  {company.summary &&
+                    company.summary.positive_points.map((positiveItem, index) => (
+                      <li key={index}>
+                        {positiveItem}
+                      </li>
+                    ))}
+                </ul>
+              </div>
+              <div className="column">
+                <h4>Negative points</h4>
+                <ul className="actions-list">
+                  {company.summary &&
+                    company.summary.negative_points.map((negativeItem, index) => (
+                      <li key={index}>
+                        {negativeItem}
+                      </li>
+                    ))}
+                </ul>
+              </div>
+              <div className="column">
+                <h4>Overall outlook</h4>
+                <p>{company.summary.overall_outlook}</p>
+              </div>
+            </div>
           </div>
         </div>
     </section>
