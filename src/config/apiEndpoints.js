c0c20ea46e1f087bb80ec27c417b21da59953c49
Venkatesh Mishra <venmishr@publicisgroupe.net>
2023-11-21 15:12:14 +0000

    host service url change
    host service url change

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -69,7 +69,7 @@ export const postAction = async (
     const response = await axios.post(
       // `${apiUrl}/message-actions`,
       //`${apiUrl}/v1/analyze`,
-      `https://3592-93-186-144-66.ngrok-free.app/v1/analyze`,
+      `http://4.157.185.216/v1/analyze`,
       // {
       //   sessionID: currentSessionID,
       //   speaker: currentSpeaker,
