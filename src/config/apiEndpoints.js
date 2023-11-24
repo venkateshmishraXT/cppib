78ab9dc955c7d498410b3a60eff49d561033f1da
Venkatesh Mishra <venmishr@publicisgroupe.net>
2023-11-24 14:21:57 +0000

    removed unused code
    removed unused code

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -117,7 +117,6 @@ export const fetchGainers = async (
     const response = await axios.post(
       // `${apiUrl}/message-actions`,
       `https://3592-93-186-144-66.ngrok-free.app/v1/analyze`,
-      //`https://4.157.185.216/v1/analyze`,
       // {
       //   sessionID: currentSessionID,
       //   speaker: currentSpeaker,
