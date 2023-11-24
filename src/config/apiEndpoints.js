8f6167741bd1eb8f088bb1f5873a28bd99197865
Venkatesh Mishra <venmishr@publicisgroupe.net>
2023-11-24 13:55:59 +0000

    reverted to ngroc
    reverted to ngroc

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -116,8 +116,8 @@ export const fetchGainers = async (
   try {
     const response = await axios.post(
       // `${apiUrl}/message-actions`,
-      //`https://3592-93-186-144-66.ngrok-free.app/v1/analyze`,
-      `https://4.157.185.216/v1/analyze`,
+      `https://3592-93-186-144-66.ngrok-free.app/v1/analyze`,
+      //`https://4.157.185.216/v1/analyze`,
       // {
       //   sessionID: currentSessionID,
       //   speaker: currentSpeaker,
