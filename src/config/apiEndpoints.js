92f48e21a720791a8e69fa51f2fc8a6b4308b2a8
Venkatesh Mishra <venmishr@publicisgroupe.net>
2023-12-01 09:45:29 +0000

    endpoint change
    endpoint change

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -69,7 +69,7 @@ export const postAction = async (
     const response = await axios.post(
       // `${apiUrl}/message-actions`,
       //`${apiUrl}/v1/analyze`,
-      `https://3592-93-186-144-66.ngrok-free.app/v1/analyze`,
+      `https://1dea-38-242-186-240.ngrok-free.app/v1/analyze`,
       //`http://4.157.49.165/v1/analyze`,
       // {
       //   sessionID: currentSessionID,
