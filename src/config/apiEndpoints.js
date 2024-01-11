c5b641bb14d58fb6727f7fed4dc503862a2fe78c
Hariom Bangari <hariom.bangari@publicissapient.com>
2024-01-11 17:10:04 +0000

    change API domain

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -67,17 +67,9 @@ export const postAction = async (
 ) => {
   try {
     const response = await axios.post(
-      // `${apiUrl}/message-actions`,
-      //`${apiUrl}/v1/analyze`,
-      //`https://3592-93-186-144-66.ngrok-free.app/v1/analyze`,
       //`http://4.157.185.216/v1/analyze`,
-      //`http://localhost:5000/v1/analyze`,
-      `http://4.157.49.165/v1/analyze`,
-      // {
-      //   sessionID: currentSessionID,
-      //   speaker: currentSpeaker,
-      //   query: newMessage,
-      // },
+      // `http://localhost:3001/v1/analyze`,
+      "https://aibot.hariom.uk/v1/analyze",
       {
         "messages": [{
           "role": currentSpeaker,
